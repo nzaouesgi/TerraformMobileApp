78e4c8224ce1224be2f61509eed2c2319a48b6a5
nzaouesgi <hakhenaton@protonmail.com>
2020-12-04 23:12:54 +0100

    added mysql database

--- main.tf
+++ main.tf
@@ -61,15 +61,58 @@ resource "azurerm_app_service_plan" "api_service_plan" {
 }
 
 resource "azurerm_app_service" "api" {
+  
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.api_service_plan.id
   https_only          = true
+  
   site_config {
     ftps_state = "Disabled"
     app_command_line = ""
     linux_fx_version = "DOCKER|vad1mo/hello-world-rest"
     always_on   = true
   }
+
+  app_settings = {
+    "DATABASE_USER" = azurerm_mysql_server.mysql.administrator_login
+    "DATABASE_PASSWORD" = azurerm_mysql_server.mysql.administrator_login_password
+    "DATABASE_HOST" = azurerm_mysql_server.mysql.fqdn
+  }
+}
+
+# MYSQL
+resource azurerm_mysql_server mysql {
+  name                = "gaming4life-mysql-server"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  administrator_login          = var.my_sql_user
+  administrator_login_password = var.my_sql_password
+
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+}
+
+# This firewall rule allows MySQL to communicate with other resources such as App services etc...
+resource azurerm_mysql_firewall_rule mysql_firewall {
+  name                = "gaming4life-mysqlfirewall"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_server.mysql.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+# This is the main application database
+resource azurerm_mysql_database database {
+  name                = "gaming4life"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_server.mysql.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
--- variables.tf
+++ variables.tf
@@ -45,3 +45,13 @@ variable "api_version" {
   default = "1"
   description = "This is used by the API management resource."
 }
+
+variable "mysql_user" {
+  type = string
+  description = "Admin user for MySQL"
+}
+
+variable "mysql_password" {
+  type = string
+  description = "Admin password for MySQL"
+}
